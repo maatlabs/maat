6646f878d0390da4c131c5239edae14a8b2e4da6
Kobby Pentangeli <76056236+kobby-pentangeli@users.noreply.github.com>
2025-12-03 09:08:40 +0000

    Implement `Token` and `TokenKind` for the lexer

---
+++ src/lexer/token.rs
@@ -0,0 +1,80 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Token<'a> {
+    pub kind: TokenKind,
+    pub literal: &'a str,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum TokenKind {
+    // Keywords
+    Let,
+    If,
+    Else,
+    True,
+    False,
+    Function,
+    Return,
+
+    // Identifiers and literals
+    Ident,
+    Int,
+
+    // Operators
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+
+    Less,
+    Greater,
+
+    Equal,
+    NotEqual,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    Invalid,
+    Eof,
+}
+
+impl TokenKind {
+    /// Maps the given input to a valid keyword,
+    /// defaulting to `Self::Ident` if no match is found.
+    #[inline]
+    pub fn lookup_keyword(k: &str) -> Self {
+        match k {
+            "let" => Self::Let,
+            "if" => Self::If,
+            "else" => Self::Else,
+            "true" => Self::True,
+            "false" => Self::False,
+            "fn" => Self::Function,
+            "return" => Self::Return,
+            _ => Self::Ident,
+        }
+    }
+}
+
+impl<'a> Token<'a> {
+    pub fn new(kind: TokenKind, literal: &'a str) -> Self {
+        Self { kind, literal }
+    }
+}
+
+impl<'a> std::fmt::Display for Token<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self.kind {
+            TokenKind::Ident => write!(f, "{}", self.literal),
+            _ => write!(f, "{:?}", self.kind),
+        }
+    }
+}
