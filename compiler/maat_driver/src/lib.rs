63a76ed0600912f9609f5930e45eb3f65b1cf2c8
Kobby Pentangeli <76056236+kobby-pentangeli@users.noreply.github.com>
2026-02-08 06:56:32 +0000

    Housekeeping

--- compiler/maat_driver/src/lib.rs
+++ compiler/maat_driver/src/lib.rs
@@ -4,8 +4,13 @@
 
 pub use maat_ast::ast::*;
 pub use maat_ast::{Node, Program};
-pub use maat_errors::{Error, EvalError, ParseError, Result};
+pub use maat_bytecode::{Bytecode, Instructions, Opcode, decode_operands, encode};
+pub use maat_codegen::Compiler;
+pub use maat_errors::{DecodeError, Error, EvalError, ParseError, Result, VmError};
 pub use maat_eval::{Env, FALSE, Hashable, NULL, Object, TRUE, define_macros, eval, expand_macros};
 pub use maat_lexer::{Lexer, Span, Token, TokenKind};
 pub use maat_parse::{Parser, TransformFn, transform};
-pub use {maat_ast, maat_errors, maat_eval, maat_lexer, maat_parse};
+pub use maat_vm::VM;
+pub use {
+    maat_ast, maat_bytecode, maat_codegen, maat_errors, maat_eval, maat_lexer, maat_parse, maat_vm,
+};
